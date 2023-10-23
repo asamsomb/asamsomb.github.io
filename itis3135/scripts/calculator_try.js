c907597c74eb3b12f42d729a371a02c6e4ffd6f4
asamsomb <aidaa.lee@gmail.com>
2023-10-23 09:22:31 -0400

    Calculator_try update js and readonly

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,14 +1,33 @@
-function clearScreen() {
-    document.getElementById("calculation").value="";
-}
+/* reference: https://www.makeuseof.com/build-a-simple-calculator-using-html-css-javascript/ */
+
+document.getElementById("clear").addEventListener("click", clearScreen);
+document.getElementById("equals").addEventListener("click", calculate);
+document.getElementById("number1").addEventListener("click", function () {display(1);});
+document.getElementById("number2").addEventListener("click", function () {display(2);});
+document.getElementById("number3").addEventListener("click", function () {display(3);});
+document.getElementById("number4").addEventListener("click", function () {display(4);});
+document.getElementById("number5").addEventListener("click", function () {display(5);});
+document.getElementById("number6").addEventListener("click", function () {display(6);});
+document.getElementById("number7").addEventListener("click", function () {display(7);});
+document.getElementById("number8").addEventListener("click", function () {display(8);});
+document.getElementById("number9").addEventListener("click", function () {display(9);});
+document.getElementById("number0").addEventListener("click", function () {display(0);});
 
 function display(value) {
-    document.getElementById("calculation").value += value;
+    const calculation = document.getElementById("calculation");
+    calculation.value += value;
+}
+
+function clearScreen() {
+    const calculation = document.getElementById("calculation");
+    calculation.value = '';
 }
 
 function calculate() {
-    var x = document.getElementById("calculation").value;
-    var y = eval(x);
-
-    document.getElementById("calculation").value = y;
+    const calculation = document.getElementById('calculation');
+    try {
+        calculation.value = eval(calculation.value);
+    } catch (error) {
+        calculation.value = 'Cannot Calculate!';
+    }
 }
